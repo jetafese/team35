8d25531a8bcfbbc885bb70b486823789fc830e45
desireepenaranda <penad@seas.upenn.edu>
2020-07-13 01:51:26 -0400

    sound options area to test/select sound to match to note

--- target/portfolio-1/script.js
+++ target/portfolio-1/script.js
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 // create empty note to render on the page 
+document.addEventListener("DOMContentLoaded", resetNoteCreation);
 
 // note class in the script file because when individual file could not call NOte constructor 
 class Note {
@@ -43,4 +44,22 @@ function noteCreation(){
     document.getElementById("note_being_created").src = current_note.imageFile;
     document.getElementById("note_being_created").style.width = '100px';
     document.getElementById("note_being_created").style.height = '100px';
+
+    // making the sound selection visible 
+    document.getElementById("sound_options").style.display = "normal";
+    console.log("just changed the display to be :normal");
+    // have method that shows the possible audio files to choose from 
+
+    // then thew file we do click we attach call the setter for the note object inorder to set it 
+
+}
+
+/* method that is called when web page loads in order to take care 
+    of ensuring everything is ready to be loaded. currently that is 
+    making sure that when the page loads we do not get access to sounds until 
+    we create a new note 
+*/
+function resetNoteCreation(){
+    // document.getElementById("sound_options").style.display = "none";  
+    console.log("came into the reset method")
 }
